181cadc6d9774b7fa415652413ea29157d3c6b71
Naved <rizvi.naved1997@gmail.com>
2022-01-29 19:57:11 +0530

    improve scanner error message

--- src/LexicalErrorListener.java
+++ src/LexicalErrorListener.java
@@ -3,8 +3,9 @@ import org.antlr.v4.runtime.*;
 public class LexicalErrorListener extends BaseErrorListener {
     @Override
     public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine,
-                                String msg, RecognitionException e) {
-        System.out.println("Scanner error: "); // TODO should append error message?
+                            String msg, RecognitionException e) {
+                                    // invalid symbol $
+        System.out.println("Scanner error: " + msg + " at line " + String.valueOf(line) + " and position " + String.valueOf(charPositionInLine)); // TODO okay to have?
         System.exit(2); // Lexical Error Found, e.g., invalid token (TODO anything more to account for?)
     }
 }

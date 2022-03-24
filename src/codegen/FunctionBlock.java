04120888e212b1d74581c4c7dcd81bbef49e5fb0
Naved <rizvi.naved1997@gmail.com>
2022-03-23 20:26:42 -0400

    naive register allocation

--- src/codegen/FunctionBlock.java
+++ src/codegen/FunctionBlock.java
@@ -15,7 +15,8 @@ import codegen.mips_instructions.MipsInstruction;
 public class FunctionBlock {
     private String functionName;
     private String returnType;
-    private LinkedHashMap<String, String> functionArgs;
+    // todo can use tuple instead
+    public LinkedHashMap<String, String> functionArgs;
     private String[] intList;
     private String[] floatList;
     private IRInstruction[] instructions;
@@ -31,12 +32,34 @@ public class FunctionBlock {
 
     public List<MipsInstruction> getNaiveMips() {
         List<InstrRegallocTuple> naiveInstrs = this.doNaiveRegisterAllocation();
-        MIPSCodeGenerator naiveMips = new MIPSCodeGenerator(this.functionName, this.intList, this.floatList, this, naiveInstrs);
+        MipsCodeGenerator naiveMips = new MipsCodeGenerator(this, this.functionName, this.intList, this.floatList, naiveInstrs);
         List<MipsInstruction> out = naiveMips.generateMipsInstructions();
         return out;
     }
 
     private List<InstrRegallocTuple> doNaiveRegisterAllocation() {
-        // TODO make sure these are ok to use
+        ArrayList<InstrRegallocTuple> instrs = new ArrayList<>();
+        for (IRInstruction instr : this.instructions) {
+            ArrayList<IRInstruction> storeInstrs = new ArrayList<>();
+            HashMap<String, String> usedRegisters = new HashMap<>();
+            int regIdx = 0;
+            for (String e : instr.useSet()) {
+                String[] loadArgs = new String[]{MipsCodeGenerator.saveRegisters.get(regIdx), e};
+                instrs.add(new InstrRegallocTuple(new Load(loadArgs), null));
+                usedRegisters.put(e, MipsCodeGenerator.saveRegisters.get(regIdx));
+                regIdx++;
+            }
+            for (String e : instr.defSet()) {
+                String[] storeArgs = new String[]{e, MipsCodeGenerator.saveRegisters.get(regIdx)};
+                storeInstrs.add(new Store(storeArgs));
+                usedRegisters.put(e, MipsCodeGenerator.saveRegisters.get(regIdx));
+                regIdx++;
+            }
+            instrs.add(new InstrRegallocTuple(instr.allocateRegisters(usedRegisters), usedRegisters));
+            for (IRInstruction storeInstr : storeInstrs) {
+                instrs.add(new InstrRegallocTuple(storeInstr, null));
+            }
+        }
+        return instrs;
     }
 }
